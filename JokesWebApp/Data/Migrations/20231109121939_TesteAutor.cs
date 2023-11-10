824646fdbbe2f21db7a284f3bcde313ac6a7466d
DiogoCarlos14 <diogocorreiacarlos@gmail.com>
2023-11-10 15:37:34 +0000

    Adicionar arquivos de projeto.

---
+++ JokesWebApp/Data/Migrations/20231109121939_TesteAutor.cs
@@ -0,0 +1,29 @@
+﻿    using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TesteAutor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Autor",
+                table: "Jokes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Autor",
+                table: "Jokes");
+        }
+    }
+}
---
+++ JokesWebApp/Models/Jokes.cs
@@ -0,0 +1,18 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+
+        public string Autor { get; set; }
+        public string JokePergunta { get; set; }
+        public string JokesRespostas { get; set; }
+
+        public Jokes() {
+            Id = 0;
+            Autor = string.Empty;
+            JokePergunta = string.Empty;
+            JokesRespostas = string.Empty;
+        }
+    }
+}
